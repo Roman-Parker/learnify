898c2d0fdd073b43bccc5f8e2a421609f05b945c
Roman Parker <roman@zoezi.se>
2022-09-15 22:03:00 +0200

    implemented generic repository patterns and specifications

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -4,6 +4,7 @@ using API.Dto;
 using AutoMapper;
 using Entity;
 using Entity.Interfaces;
+using Entity.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -11,12 +12,12 @@ namespace API.Controllers
     public class CategoriesController : BaseController
     {
         private readonly IGenericRepository<Category> _repository;
-        private readonly IMapper _mapper;
 
+        private readonly IMapper _mapper;
         public CategoriesController(IGenericRepository<Category> repository, IMapper mapper)
         {
-            _mapper = mapper;
             _repository = repository;
+            _mapper = mapper;
 
         }
 
@@ -32,7 +33,10 @@ namespace API.Controllers
 
         public async Task<ActionResult<CategoryDto>> GetCategory(int id)
         {
-            var category = await _repository.GetByIdAsync(id);
+            var spec = new CategoriesWithCoursesSpecification(id);
+
+            var category = await _repository.GetEntityWithSpec(spec);
+
             return _mapper.Map<Category, CategoryDto>(category);
         }
     }
--- API/Controllers/CoursesController.cs
+++ API/Controllers/CoursesController.cs
@@ -5,27 +5,33 @@ using API.Dto;
 using AutoMapper;
 using Entity;
 using Entity.Interfaces;
+using Entity.Specifications;
+using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace API.Controllers
 {
     public class CoursesController : BaseController
     {
-        private readonly IGenericRepository<Course> _repository;
+
         private readonly IMapper _mapper;
+        private readonly IGenericRepository<Course> _repository;
 
         public CoursesController(IGenericRepository<Course> repository, IMapper mapper)
         {
-            _mapper = mapper;
             _repository = repository;
-
+            _mapper = mapper;
         }
 
         [HttpGet]
 
         public async Task<ActionResult<IReadOnlyList<CourseDto>>> GetCourses()
         {
-            var courses = await _repository.ListAllAsync();
+            var spec = new CoursesWithCategoriesSpecification();
+
+            var courses = await _repository.ListWithSpec(spec);
             return Ok(_mapper.Map<IReadOnlyList<Course>, IReadOnlyList<CourseDto>>(courses));
         }
 
@@ -33,9 +39,12 @@ namespace API.Controllers
 
         public async Task<ActionResult<CourseDto>> GetCourse(Guid id)
         {
-            var course = await _repository.GetByIdAsync(id);
+            var spec = new CoursesWithCategoriesSpecification(id);
+
+            var course = await _repository.GetEntityWithSpec(spec);
 
             return _mapper.Map<Course, CourseDto>(course);
+
         }
     }
 }
