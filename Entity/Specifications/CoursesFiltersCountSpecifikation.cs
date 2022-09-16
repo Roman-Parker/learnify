3ba2d996ef638384b4fc4e060cd54eb88464a1a5
Roman Parker <roman@zoezi.se>
2022-09-16 14:55:05 +0200

    finished pagination

---
+++ Entity/Specifications/CoursesFiltersCountSpecifikation.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq.Expressions;
+
+namespace Entity.Specifications
+{
+    public class CoursesFiltersCountSpecification : BaseSpecification<Course>
+    {
+        public CoursesFiltersCountSpecification(CourseParams courseParams) : base( x =>
+            !courseParams.CategoryId.HasValue || x.CategoryId == courseParams.CategoryId
+        )
+        {
+        }
+    }
+}
