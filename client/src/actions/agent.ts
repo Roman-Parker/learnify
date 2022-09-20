7bf698ed358801c9f7a06dbec6e2a960a17ce41e
Roman Parker <roman@zoezi.se>
2022-09-20 10:55:21 +0200

    refactured frontend

--- client/src/actions/agent.ts
+++ client/src/actions/agent.ts
@@ -1,23 +1,27 @@
-import axios, { AxiosResponse } from 'axios';
-import {Course} from "../models/course";
+import axios, { AxiosResponse } from 'axios'
+import { Course } from '../models/course'
+import { PaginatedCourse } from '../models/paginatedCourse'
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'http://localhost:5000/api'
 
-const responseBody =  <T> (response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data
 
 const requests = {
-  get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-  post: <T> (url: string, body: {}) => axios.post<T> (url, body).then(responseBody),
-  put: <T> (url: string, body: {}) => axios.put<T> (url, body).then(responseBody),
-  del:<T>  (url: string) => axios.delete<T> (url).then(responseBody),
-};
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+}
 
 const Courses = {
-  list: () => requests.get<Course[]>('/courses'),
-};
+  list: () => requests.get<PaginatedCourse>('/courses'),
+  getById: (id: string) => requests.get<Course>(`courses/${id}`),
+}
+
 
 const agent = {
   Courses,
-};
+}
 
-export default agent;
+export default agent
--- client/src/models/course.ts
+++ client/src/models/course.ts
@@ -1,8 +1,19 @@
 export interface Course {
-    is: string;
+    id: string;
     title: string;
     price: number;
     instructor: string;
     image: string;
     rating: number;
-}
+    description: string;
+    language: string;
+    category: string;
+    learnings: string[] | [];
+    requirements: string[] | [];
+    level: string;
+    students: number;  
+    subTitle: string;
+  }
+  
+  
+  
