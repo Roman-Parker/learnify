dcffa1e388de421be370b581ef5dbff0d5c463dd
Roman Parker <roman@zoezi.se>
2022-09-22 11:57:10 +0200

    added syncronous calls

--- client/src/redux/slice/basketSlice.ts
+++ client/src/redux/slice/basketSlice.ts
@@ -1,13 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { act } from '@testing-library/react';
+import agent from '../../actions/agent';
 import { Basket } from '../../models/basket';
 
 interface BasketState {
-  basket: Basket | null;
+  basket: Basket | null | undefined;
+  status: string;
 }
 
 const initialState: BasketState = {
   basket: null,
+  status: 'idle',
 };
+
+export const addBasketItemAsync = createAsyncThunk<Basket | undefined, {courseId: string}>(
+    'basket/addBasketItemAsync', 
+    async({courseId}) => {
+        try {
+            return await agent.Baskets.addItem(courseId)
+        } catch (error) {
+            console.log(error)
+        }
+})
+
 
 export const basketSlice = createSlice({
   name: 'basket',
@@ -25,6 +40,15 @@ export const basketSlice = createSlice({
       state.basket?.items.splice(itemIndex, 1);
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(addBasketItemAsync.pending, (state, action) => {
+        state.status = 'pending'
+    })
+    builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+        state.basket = action.payload;
+        state.status = 'idle'
+    })
+  }
 });
 
 export const { setBasket, removeItem } = basketSlice.actions;
