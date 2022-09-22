8b22b372e2fb439ac4a0c25e0d5ecc7913d37d35
Roman Parker <roman@zoezi.se>
2022-09-22 10:52:17 +0200

    added basketslice

--- client/src/redux/store/configureStore.ts
+++ client/src/redux/store/configureStore.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { basketSlice } from "../slice/basketSlice";
 import { loginSlice } from "../slice/loginSlice";
 
 
 export const store = configureStore({
   reducer: {
     login: loginSlice.reducer,
+    basket: basketSlice.reducer,
   },
 });
 
