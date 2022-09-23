1577eeb4e4281dc49df56b8322d51a4a9b1a310a
Roman <Roman@DESKTOP-8ACHKI7>
2022-09-23 14:25:06 +0200

    added tokenservice

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,12 @@ namespace API
 
         // This method gets called by the runtime. Use this method to add services to the container.
            public void ConfigureServices(IServiceCollection services)
-        {
+            {
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+            });
+
             services.AddIdentityCore<User>()
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<StoreContext>();
