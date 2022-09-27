3c815b988ef17952d58e4585d435a275b870a4f7
Roman Parker <roman@zoezi.se>
2022-09-27 15:03:27 +0200

    added lectures controller

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using API.Dto;
 using AutoMapper;
 using Entity;
@@ -10,15 +11,8 @@ namespace API.Helpers
         {
             CreateMap<Course, CourseDto>()
             .ForMember(c => c.Category, o => o.MapFrom(s => s.Category.Name));
-
             CreateMap<Learning, LearningDto>();
-
             CreateMap<Requirement, RequirementDto>();
-
-            CreateMap<Category, CategoryDto>();
-
-            CreateMap<Category, CategoriesDto>();
-
             CreateMap<Basket, BasketDto>();
             CreateMap<BasketItem, BasketItemDto>()
             .ForMember(b => b.CourseId, o => o.MapFrom(c => c.Course.Id))
@@ -26,7 +20,9 @@ namespace API.Helpers
             .ForMember(b => b.Price, o => o.MapFrom(c => c.Course.Price))
             .ForMember(b => b.Image, o => o.MapFrom(c => c.Course.Image))
             .ForMember(b => b.Instructor, o => o.MapFrom(c => c.Course.Instructor));
-
-        }
+            CreateMap<Section, SectionDto>()
+            .ForMember(s => s.SectionName, o => o.MapFrom(c => c.Name));
+            CreateMap<Lecture, LectureDto>();
+            }
     }
 }
---
+++ Infrastructure/Migrations/20220927123626_CurrentLectureAdded.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class CurrentLectureAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7e7fa438-7251-4b38-97f5-6f6473e2edd6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d22cb5ac-7bf6-45d6-97ec-dbd717046d10");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentLecture",
+                table: "UserCourses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c26e12ff-fc72-4329-9fa4-d3230e9b8789", "5fa6d5fa-c600-4022-ba4c-4d1aba967dd4", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "28334137-47a5-4522-aea3-3819173a89d9", "de6a5ef8-befe-419e-ac3b-8e1323d8ee01", "Instructor", "INSTRUCTOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "28334137-47a5-4522-aea3-3819173a89d9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c26e12ff-fc72-4329-9fa4-d3230e9b8789");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentLecture",
+                table: "UserCourses");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "7e7fa438-7251-4b38-97f5-6f6473e2edd6", "34910d7f-6573-49db-9cbc-3e0f2570e1f5", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d22cb5ac-7bf6-45d6-97ec-dbd717046d10", "fd2db3e5-c764-40aa-a9a2-2cd17523a29a", "Instructor", "INSTRUCTOR" });
+        }
+    }
+}
