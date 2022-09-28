684dda3b199dd30e64c67ad1f38492aff9e83514
Roman Parker <roman@zoezi.se>
2022-09-28 15:33:27 +0200

    added instructor page

--- client/src/components/UserMenu.tsx
+++ client/src/components/UserMenu.tsx
@@ -1,17 +1,20 @@
-import { Dropdown, Menu } from "antd";
-import { useDispatch } from "react-redux";
-import { Link, useHistory } from "react-router-dom";
-import { removeBasket } from "../redux/slice/basketSlice";
-import { signOut } from "../redux/slice/userSlice";
+import { Dropdown, Menu } from 'antd';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { removeBasket } from '../redux/slice/basketSlice';
+import { signOut } from '../redux/slice/userSlice';
+import { useAppSelector } from '../redux/store/configureStore';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const { user } = useAppSelector((state) => state.user);
+
   const signout = () => {
     dispatch(signOut());
     dispatch(removeBasket());
-    history.push("/");
+    history.push('/');
   };
 
   const menu = (
@@ -19,6 +22,11 @@ const UserMenu = () => {
       <Menu.Item>
         <Link to="/profile">Profile</Link>
       </Menu.Item>
+      {user?.roles?.includes('Instructor') && (
+        <Menu.Item>
+          <Link to="/instructor">Instructor</Link>
+        </Menu.Item>
+      )}
       <Menu.Item>
         <div onClick={signout}>Logout</div>
       </Menu.Item>
@@ -32,3 +40,5 @@ const UserMenu = () => {
 };
 
 export default UserMenu;
+
+
